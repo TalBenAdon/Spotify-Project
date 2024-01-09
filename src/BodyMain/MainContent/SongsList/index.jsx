368fa2ef410f4035697b863a4458d9ad78598711
Tal Ben Adon <talbenadon2@gmail.com>
2024-01-10 01:36:45 +0200

    another commit

---
+++ src/BodyMain/MainContent/SongsList/index.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'
+import Song from './Song'
+
+
+
+export default function SongsList() {
+    const [useSong, setUseSong] = useState('')
+
+
+    return (
+        <div>
+
+
+        </div>
+    )
+}
+
+
+// REMEMBER WHAT ZVI DID WITH THE GIRL IN FRONT OF ME. AXIOS.REQUEST(OPTIONS) AND THEN DATA WITHIN DATA.
---
+++ src/BodyMain/MainContent/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './styles.module.css'
+export default function MainContent() {
+    return (
+        <div className={styles.MainContent}>
+            s
+        </ div>
+    )
+}
